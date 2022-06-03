6f8f1677142769fb1c087425d3de85b7188487b7
ExchangeRxzy <106664844+ExchangeRxzy@users.noreply.github.com>
2022-06-03 14:08:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,12 +14,12 @@ global.APIs = {
 
 // Apikey Website Api
 global.APIKeys = {
-	'https://zenzapis.xyz': 'Your Key',
+	'https://zenzapis.xyz': 'bbbff852340d',
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
+global.owner = ['6288287037007','6283804678512','628979079959']
+global.premium = ['6288287037007']
 global.packname = 'Hisoka Morrow'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
